1f95a1822095945d65fe70f087457eb9d5ec3ca6
mary-jiang <mary.jiang2020@gmail.com>
2021-07-14 10:54:41 -0400

    user log in is now persisted

--- FBUOriginalApp/SceneDelegate.m
+++ FBUOriginalApp/SceneDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "SceneDelegate.h"
+#import <Parse/Parse.h>
 
 @interface SceneDelegate ()
 
@@ -18,6 +19,12 @@
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+    
+    // checks if there is currently a user logged in (PFUser.current user is NOT null), if user is logged in do not show log in and instead show feed
+    if (PFUser.currentUser) {
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        self.window.rootViewController = [storyboard instantiateViewControllerWithIdentifier:@"TabBarController"];
+    }
 }
 
 
