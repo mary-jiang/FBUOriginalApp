dd407818098eb9f049c0b870f958963779d1006f
mary-jiang <mary.jiang2020@gmail.com>
2021-07-16 14:04:04 -0400

    refactored API requests to eliminate egregious code reuse

--- FBUOriginalApp/API/APIManager.m
+++ FBUOriginalApp/API/APIManager.m
@@ -19,79 +19,19 @@
 }
 
 - (void)exchangeCodeForTokenWithCompletion:(NSString *)code completion:(void(^)(NSDictionary *, NSError *))completion{
-    // get client id and client secret from Keys.plist
-    NSString *path = [[NSBundle mainBundle] pathForResource: @"Keys" ofType: @"plist"];
-    NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile: path];
-    NSString *key = [dict objectForKey: @"client_id"];
-    NSString *secret = [dict objectForKey:@"client_secret"];
-    
-    // encode client id and client secret in a base 64 encoded string
-    NSString *clientIDAndSecretString = [NSString stringWithFormat:@"%@:%@", key, secret];
-    NSData *clientIDAndSecretData = [clientIDAndSecretString dataUsingEncoding:NSUTF8StringEncoding];
-    NSString *clientIDandSecretEncoded = [clientIDAndSecretData base64EncodedStringWithOptions:0];
-    
     // create data for request body parameters
     NSString *queryParameters = [NSString stringWithFormat:@"grant_type=authorization_code&code=%@&redirect_uri=fbuoriginalapp://", code];
     NSData *queryData = [queryParameters dataUsingEncoding:NSUTF8StringEncoding];
     
-    NSURL *url = [NSURL URLWithString:@"https://accounts.spotify.com/api/token"];
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
-    [request setHTTPMethod:@"POST"];
-    // set body data
-    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
-    [request setHTTPBody:queryData];
-    // set header data
-    [request setValue:[NSString stringWithFormat:@"Basic %@", clientIDandSecretEncoded] forHTTPHeaderField:@"Authorization"];
-    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
-    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-           if (error != nil) {
-               NSLog(@"%@", [error localizedDescription]);
-               completion(nil, error);
-           }
-           else {
-               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
-               completion(dataDictionary, nil);
-           }
-       }];
-    [task resume];
+    [self baseTokenRequestWithCompletion:queryData completion:completion];
 }
 
 - (void)refreshTokenWithCompletion:(NSString *)refreshToken completion:(void(^)(NSDictionary *, NSError *))completion {
-    // get client id and client secret from Keys.plist
-    NSString *path = [[NSBundle mainBundle] pathForResource: @"Keys" ofType: @"plist"];
-    NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile: path];
-    NSString *key = [dict objectForKey: @"client_id"];
-    NSString *secret = [dict objectForKey:@"client_secret"];
-    
-    // encode client id and client secret in a base 64 encoded string
-    NSString *clientIDAndSecretString = [NSString stringWithFormat:@"%@:%@", key, secret];
-    NSData *clientIDAndSecretData = [clientIDAndSecretString dataUsingEncoding:NSUTF8StringEncoding];
-    NSString *clientIDandSecretEncoded = [clientIDAndSecretData base64EncodedStringWithOptions:0];
-    
     // create data for reuqest body parameters
     NSString *queryParameters = [NSString stringWithFormat:@"grant_type=refresh_token&refresh_token=%@", refreshToken];
     NSData *queryData = [queryParameters dataUsingEncoding:NSUTF8StringEncoding];
     
-    NSURL *url = [NSURL URLWithString:@"https://accounts.spotify.com/api/token"];
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
-    [request setHTTPMethod:@"POST"];
-    // set body data
-    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
-    [request setHTTPBody:queryData];
-    // set header data
-    [request setValue:[NSString stringWithFormat:@"Basic %@", clientIDandSecretEncoded] forHTTPHeaderField:@"Authorization"];
-    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
-    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-           if (error != nil) {
-               NSLog(@"%@", [error localizedDescription]);
-               completion(nil, error);
-           }
-           else {
-               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
-               completion(dataDictionary, nil);
-           }
-       }];
-    [task resume];
+    [self baseTokenRequestWithCompletion:queryData completion:completion];
 }
 
 - (void)searchSpotifyWithCompletion:(NSString *)query type:(NSString *)type authorization:(NSString *)authorization completion:(void(^)(NSDictionary *, NSError *))completion {
@@ -100,9 +40,35 @@
     // construct request
     NSString *urlString = [NSString stringWithFormat:@"https://api.spotify.com/v1/search?q=%@&type=%@", encodedQuery, type];
     NSURL *url = [NSURL URLWithString:urlString];
+    [self baseAPIGetRequestWithCompletion:url authorization:authorization completion:completion];
+}
+
+- (void)getTopicWithCompletion: (NSString *)spotifyId type:(NSString *)type authorization:(NSString *)authorization completion:(void(^)(NSDictionary *, NSError *))completion {
+    NSString *urlString = [NSString stringWithFormat:@"https://api.spotify.com/v1/%@s/%@", type, spotifyId];
+    NSURL *url = [NSURL URLWithString:urlString];
+    [self baseAPIGetRequestWithCompletion:url authorization:authorization completion:completion];
+}
+
+- (void)baseTokenRequestWithCompletion: (NSData *)data completion:(void(^)(NSDictionary *, NSError *))completion {
+    // get client id and client secret from Keys.plist
+    NSString *path = [[NSBundle mainBundle] pathForResource: @"Keys" ofType: @"plist"];
+    NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile: path];
+    NSString *key = [dict objectForKey: @"client_id"];
+    NSString *secret = [dict objectForKey:@"client_secret"];
+
+    // encode client id and client secret in a base 64 encoded string
+    NSString *clientIDAndSecretString = [NSString stringWithFormat:@"%@:%@", key, secret];
+    NSData *clientIDAndSecretData = [clientIDAndSecretString dataUsingEncoding:NSUTF8StringEncoding];
+    NSString *clientIDandSecretEncoded = [clientIDAndSecretData base64EncodedStringWithOptions:0];
+    
+    NSURL *url = [NSURL URLWithString:@"https://accounts.spotify.com/api/token"];
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
-    [request setHTTPMethod:@"GET"];
-    [request setValue:[NSString stringWithFormat:@"Bearer %@", authorization] forHTTPHeaderField:@"Authorization"];
+    [request setHTTPMethod:@"POST"];
+    // set body data
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+    [request setHTTPBody:data];
+    // set header data
+    [request setValue:[NSString stringWithFormat:@"Basic %@", clientIDandSecretEncoded] forHTTPHeaderField:@"Authorization"];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
            if (error != nil) {
@@ -117,9 +83,7 @@
     [task resume];
 }
 
-- (void)getTopicWithCompletion: (NSString *)spotifyId type:(NSString *)type authorization:(NSString *)authorization completion:(void(^)(NSDictionary *, NSError *))completion {
-    NSString *urlString = [NSString stringWithFormat:@"https://api.spotify.com/v1/%@s/%@", type, spotifyId];
-    NSURL *url = [NSURL URLWithString:urlString];
+- (void)baseAPIGetRequestWithCompletion: (NSURL *) url authorization: (NSString *) authorization completion:(void(^)(NSDictionary *, NSError *))completion {
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     [request setHTTPMethod:@"GET"];
     [request setValue:[NSString stringWithFormat:@"Bearer %@", authorization] forHTTPHeaderField:@"Authorization"];
