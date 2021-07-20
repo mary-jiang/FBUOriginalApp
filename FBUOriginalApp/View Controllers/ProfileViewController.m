0e68140737a2ca32013b7292a13eee9f0d2a09f7
mary-jiang <mary.jiang2020@gmail.com>
2021-07-20 17:46:41 -0400

    added function that can produce a frequency list of genres the user listens to and can sort the genres from most frequent to least frequent
    this data only comes from the Top Artists as spotify only has genre information for artists and literally nothing else

--- FBUOriginalApp/View Controllers/ProfileViewController.m
+++ FBUOriginalApp/View Controllers/ProfileViewController.m
@@ -254,6 +254,39 @@
     [self dismissViewControllerAnimated:true completion:nil];
 }
 
+- (void)mostListenedToGenres {
+    NSMutableDictionary *genreFrequencyList = [NSMutableDictionary dictionary];
+    // only artists have genre information
+    [[APIManager shared] getTopArtistsWithCompletion:self.token numberOfArtists:50 completion:^(NSDictionary *results, NSError *error) {
+        if (error != nil) {
+            NSLog(@"%@", error.localizedDescription);
+        } else {
+            NSArray *artists = results[@"items"];
+            for (NSDictionary *artist in artists) {
+                NSArray *genres = artist[@"genres"];
+                for (NSString *genre in genres) {
+                    if (genreFrequencyList[genre] != nil) {
+                        NSNumber *value = genreFrequencyList[genre];
+                        genreFrequencyList[genre] = [NSNumber numberWithInt:[value intValue] + 1];
+                    } else {
+                        [genreFrequencyList setValue:[NSNumber numberWithInt:1] forKey:genre];
+                    }
+                }
+            }
+            NSArray *sortedGenres = [genreFrequencyList keysSortedByValueUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+                if ([obj1 intValue] > [obj2 intValue]) {
+                    return NSOrderedAscending;
+                } else if ([obj1 intValue] < [obj2 intValue]) {
+                    return NSOrderedDescending;
+                } else {
+                    return NSOrderedSame;
+                }
+            }];
+            NSLog(@"%@", sortedGenres);
+        }
+    }];
+}
+
 - (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
     UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
     
